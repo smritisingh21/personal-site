18a1d7d581b4f6e4f3fa43586c95651bf01c235d
Smriti Singh <smritiiisinghh@gmail.com>
2026-09-06 22:13:12 +0530

    New blog updated

--- src/Blogs.js
+++ src/Blogs.js
@@ -62,63 +62,31 @@ export const blogs = [
     }
     ],
   },
-//   {
-//     id: 2,
-//     title: 'How did I get here?',
-//     date: 'Mar 29, 2026',
-//     readTime: '',
-//     tags: [],
-//     link : "/blog/2",
-//     content: [
-//     {
-//       type: 'p',
-//       text: `I’ve been juggling a lot of things lately. 
-//     Applying to internships and jobs, trying to work on my projects, and learning concepts outside of them. \n
-//     Time has been weird. \n
-//     As I recall my school days, I see similarities and stark differences between that girl and who I am now. 
-//     I wanted to be everything: a pilot, an interior designer, a product designer. \n
 
-//     Lately, I’ve started feeling that maybe I am not a "naturally" creative person. 
-//     I am decent at copying art, but I struggle to generate something from scratch. 
-//     Is that a bad thing? I have no clue. 
-//     Maybe I do random sketches just because it feels good; an inner part of me feels validated as a creative person. 
-//     I was active in school—art competitions, noticeboard decor—but it never felt as polished as what others did. \n
+  {
+    id: 2,
+    title: 'Things are still unclear, yet I remain hopeful',
+    date: 'September 6, 2026',
+    readTime: '',
+    tags: [],
+    link : "/blog/2",
+    content: [
+    {
+      type: 'p',
+      text: `It’s been a long time since a wrote something and clarified my thoughts. I guess I only do it when I am truly clueless, yet not in a mood to give up.Placements season is at peak , and almost 150 students have already been placed. And here I am , without a single opportunity to even prove myself.When people said placements are a matter of luck , I thought to myself “Huh! How true can that be?”. But now I know it better than anyone, probably. A lot of things are happening around me and I feel nothing is in my hand right now.I’ve spent my last 3 years to be really good enough to secure a job and let nothing come in my way. 
+       
+       My friend got placed a few days back too, and trust me , that guy deserves even more. What really confused me and left me thinking was the selection process.It was a hackathon and we were a team of 2. I think I had a great discussion with the HR guys and really put a lot of effort in understanding and developing the entire solution from scratch. However, I was not shortlisted for the interview. Maybe I was too naive to say something I shouldn’t have, or maybe the selection criteria was restrictive.I guess they did not seem me fit for the job, or maybe I should not have blabbered about how I explored so many domains along with my technical skills.
+       
+       I still went to the interview with my friend to support him, because this was the very first interview call for him too (Yes, none of us ever got any interview calls for about 2 months and he is a very strong candidate). I got a gist of what everyone was being asked and realized that I really could’ve cracked it, had I got a chance to just appear for the interview.
 
-//     I joined art classes and discovered a love for watercolors. 
-//     I learned techniques but was always too shy to experiment. 
-//     The first time I earned money, I spent it on art supplies. \n
-//     I even started an Instagram account, @smriti.made.these—don’t judge, I know they aren’t that "good." 
-//     I thought about pursuing a creative career, but in a country where survival is a luxury, art helps the soul survive but doesn't always pay the bills. \n
+       But life, doesn’t work the way we want it too. Now the only thing that scares me a little is that, what if I keep sharpening my skills and no opportunity knocks?However , I truly want to refrain myself from complaining and shift my focus to be better, because the more you learn , the more you realize how less you know.I have dived deep enough to realize this, and there is no stopping now.I am extremely glad for my friend to have secured this opportunity, as he is an extremely sharp mind. At least one of us has something to fall back on, that too in a great company.
 
-//     I didn’t want to limit myself. 
-//     I thought, "Maybe I can pursue a STEM path; the pay is great." 
-//     Ended up taking CS in high school and kept continuing with my art. Meanwhile, I was also taking art classes for two 7-8 years old kids . Had a lot of fun  and enjoyed every moment with them.\n
-//     After high school, I was lost. Everything was interesting. 
-//     I felt I could get into tech without an expensive engineering degree if I hustled. 
-//     I learned video editing, design, and branding in a cohort of talented people. \n
 
-//     I ended up at a Tier-3 engineering college in Bengaluru. 
-//     I joined a senior-led agency as a video editor and freelanced as an illustrator for books. 
-//     Parallelly, I was learning to code. 
-//     I started liking tech—understanding how things work internally. 
-//     Eventually, I focused on web development, pausing the other creative hustles to manage college attendance. \n
+       It hits you a bit differently when you know you could’ve easily got something and were prepared more than enough for it.
 
-//     I fell in love with frontend and React, but I didn't stop there. 
-//     I moved into backend, built CRUD applications, and started diving deep into documentation. 
-//     Now, I’m freelancing as an engineer. 
-//     I provide web services and design websites for clients. 
-//     Somehow, those creative pursuits aligned with my technical interests. 
-//     I get to do the "fun stuff" with the benefit of building functional products and earning well. \n
-
-//     I still feel insecure that I’m not "perfect" at anything—art or tech. 
-//     But maybe imperfections push us to explore beyond a niche and discover something entirely new. 
-//     I’m loving the mashup of both fields. \n
-//     Not every day is perfect; sometimes I’m tired or lazy, but that’s just life. 
-//     I’m still not limiting myself. \n
-//     Tech is an ocean, and I have a lot more swimming to do. \n
-//     Maybe my average creative mind has finally found a sense of peace here.`
-//     },
-// ]
-// }
+       I will keep grinding the way I always have been, now that I am slowly getting over this feeling of missing the mark by an inch.Let us see how things unfold and whether I am lucky enough to at least get an opportunity to prove myself.`
+    },
+  ]
+}
 
 ];
